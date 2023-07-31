a0dc72f0ccaad939e92a53aaf72ab6aa199512bb
mgr514 <mgr514@mun.ca>
2023-07-30 22:06:07 -0230

    started javascript added more to css

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const ratingInputs = document.querySelector(['rating-inputs'])
+const thankYouState = document.querySelector(['thank-you-state'])
+
+ratingInputs.forEach((input) =>{
+input.addEventListener('click')})
+
+function      (event) {}
+    const 
+    console.log('')
