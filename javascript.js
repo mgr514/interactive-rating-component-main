1bc2018dd91c3b07c307fa0c3de85e833206e693
mgr514 <mgr514@mun.ca>
2023-08-07 19:35:11 -0230

    custom radio buttons

--- javascript.js
+++ javascript.js
@@ -1,9 +1,27 @@
+
+const submit_button = document.querySelector(['rating-submit'])
 const ratingInputs = document.querySelector(['rating-inputs'])
 const thankYouState = document.querySelector(['thank-you-state'])
+const rating_buttons = document.querySelectorAll('input')
+const score = document.getElementById('score')
 
-ratingInputs.forEach((input) =>{
-input.addEventListener('click')})
 
-function      (event) {}
-    const 
-    console.log('')
+let rating_value = '0';
+
+
+
+const checkRatingValue = () => {
+    rating_buttons.forEach {
+        (input) => {
+            if(input.checked) {
+                rating_value = input.value
+            }}}}
+
+
+submit_button.addEventListener('click', () =>
+    {
+    ratingInputs.classList.add('hidden')
+    thankYouState.classList.remove('hidden')
+    checkRatingValue()
+    score.innerHTML = rating.value
+});
